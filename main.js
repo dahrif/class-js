21dd688361b8e3594dd76eca1a458a268d8823c9
dahrif <dahri.fatima.dev@gmail.com>
2021-06-14 11:23:05 +0100

    vehicule

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-class personnes {
-    constructor (nom , prenom , age){
-        this.nom = nom
-        this.prenom = prenom
-        this.age = age
+// class personnes {
+//     constructor (nom , prenom , age){
+//         this.nom = nom
+//         this.prenom = prenom
+//         this.age = age
 
-    }
+//     }
 
-    getFullName() {
-        return 'hello' + ' ' + this.nom 
+//     getFullName() {
+//         return 'hello' + ' ' + this.nom + " "+ this.prenom
+//     }
+// }
+
+// let personne = new personnes ('dahri' , 'fatima' , '26')
+// alert(personne.nom);
+
+// console.log(personne.getFullName())
+
+
+class vehicule {
+    constructor (marque , model , année , nbporte , nbroue ){
+        this.marque = marque
+        this.model = model
+        this.année = année
+        this.nbporte = nbporte
+        this.nbroue = nbroue
     }
 }
 
-let personne = new personnes ('dahri' , 'fatima' , '26')
+class voiture extends vehicule {
+    constructor (marque , model , année ){
+        super (marque , model , année , 4 , 4)
+    }
+}
 
-
-console.log(personne.getFullName())
+var voitures = new voiture ('AUDI' , 'A1' , '2020')
+console.log(voitures.marque)
