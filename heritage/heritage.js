006fd74854a96bbce1f410ae7a7539d159f85861
dahrif <dahri.fatima.dev@gmail.com>
2021-06-14 13:10:58 +0100

    public

--- heritage/heritage.js
+++ heritage/heritage.js
@@ -1,18 +1,24 @@
 class vehicule {
-    constructor (marque , model , année , nbporte , nbroue ){
-        this.marque = marque
-        this.model = model
-        this.année = année
+nbporte;
+nbroue;
+
+    constructor ( nbporte , nbroue ){
+    
         this.nbporte = nbporte
         this.nbroue = nbroue
     }
 }
 
 class voiture extends vehicule {
-    constructor (marque , model , année ){
-        super (marque , model , année , 4 , 4)
+    marque;
+    constructor (marque){
+        super(4,4)
+        this.marque = marque
+       
     }
 }
 
-var voitures = new voiture ('AUDI' , 'A1' , '2020')
-console.log(voitures.marque)
+
+
+var voitures = new voiture ('AUDI')
+console.log(voitures)
